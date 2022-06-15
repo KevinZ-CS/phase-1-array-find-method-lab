1954c8e11f07f41123c47f34e7cf6095f66ac2f0
Kevin Zheng <kevzheng27@gmail.com>
2022-06-15 16:26:11 -0400

    commit tree f725b72f0494640a7fdc6df26d5426aa62542fea

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    const var1 = record.find(element => element.result ==='W'); 
+    if (var1 && var1.result === 'W'){
+    return var1.year}
+    return undefined
+}
